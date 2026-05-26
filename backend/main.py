b239e3f829939eaeca7deeb49775d516252859dd
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-05-27 01:36:30 +0600

    Enhance main.py with dashboard and login endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from auth import check_user
 
 app = FastAPI(title="ISP CLOUD SYSTEM")
 
 @app.get("/")
 def home():
-    return {"status": "LIVE ISP SYSTEM"}
+    return {
+        "status": "LIVE",
+        "system": "ISP CLOUD"
+    }
+
+@app.get("/dashboard")
+def dashboard():
+    return {
+        "online_users": 120,
+        "offline_users": 25,
+        "revenue": 50000
+    }
+
+@app.post("/login")
+def login(username: str, password: str):
+
+    user = check_user(username, password)
+
+    return {
+        "status": "SUCCESS",
+        "role": user["role"]
+    }
